bde9eac033f0d3a1f3b7c36c84f69c62cf769891
Sandro Junior <60121783+sandromjr@users.noreply.github.com>
2020-02-27 17:14:51 -0300

    Add_Rodape_&_Comentarios

--- Stored_result_From_Stored_procedure_MySQL.py
+++ Stored_result_From_Stored_procedure_MySQL.py
@@ -1,47 +1,81 @@
-import datetime
-import mysql.connector
-from openpyxl import Workbook
-from mysql.connector import Error
+#Cria a função que conecta ao banco de dados, realiza a consulta a partir de _
+#uma Stored Procedure e o resultado é armazenado em uma planilha excel. _
+#O arquivo será salvo no diretório escolhido
 
-data = (datetime.date(2020, 1, 31))
-print(f"Date: {data} SELECTED")
+def posicao_carteira_xl():
+    import datetime
+    import mysql.connector
+    from openpyxl import Workbook
+    from mysql.connector import Error
 
-header = [
-    ("Data", "Cliente", "Descricao", "Quantidade", "Preco_Unitario", "Valor_de_Mercado")
-]
+#Declara a data que será realizada a pesquisa
+    data = (datetime.date(2020, 1, 31))
+#Armazena o horário e data que ao início da função
+    time = datetime.datetime.now()
 
-arquivo_excel = Workbook()
-planilha1 = arquivo_excel.active
-print("Excel File: OPENED")
+#Controle em Run
+    print(f"Date: {data} SELECTED")
 
-try:
-    connection = mysql.connector.connect(host='127.0.0.1',
-                                         database='***',
-                                         user='***',
-                                         password='***')
-    print("Database: CONNECTED")
-    cursor = connection.cursor()
-    cursor.callproc('MJ_posicao_carteira', (data,))
-    print("Stored Procedure: CALLED SUCCESSFULLY")
+#Declara o cabeçalho da planilha
+    header = [
+        ("Data", "Cliente", "Descricao", "Quantidade", "Preco_Unitario", "Valor_de_Mercado")
+    ]
 
-    for linha in header:
-        planilha1.append(linha)
+#Inicia o arquivo excel
+    arquivo_excel = Workbook()
+    planilha1 = arquivo_excel.active
 
-    for j in cursor.stored_results():
-        for row in j:
-            planilha1.append(row)
+#Controle em Run
+    print("Excel File: OPENED")
 
-    print("Downloading Datas")
+#Inicia a conexão
+    try:
+        connection = mysql.connector.connect(host='127.0.0.1',
+                                         database='lts1',
+                                         user='sandro',
+                                         password='Smj091050#')
+#Controle em Run
+        print("Database: CONNECTED")
+        cursor = connection.cursor()
+
+#Chama a Stored Procedure (Com args)
+        cursor.callproc('MJ_posicao_carteira', (data,))
+
+#Controle em Run
+        print("Stored Procedure: CALLED SUCCESSFULLY")
+
+#Copia as informações do cabeçalho para a primeira linha do Excel
+        for linha in header:
+            planilha1.append(linha)
+
+#Copia todos os dados da consulta no Excel, abaixo do cabeçalho
+        for j in cursor.stored_results():
+            for row in j:
+                planilha1.append(row)
+
+#Insere os dados de atualização ao final da tabela
+        planilha1.append([("")])
+        planilha1.append([(time.strftime("UPDATE %d/%m/%Y %H:%M:%S"))])
+
+#Controle em Run
+        print("--- Downloading Datas ---")
 
 
-#except Error as e:
-#    print("Error reading data from MySQL table", e)
-finally:
-    if (connection.is_connected()):
-        connection.close()
-        cursor.close()
-        print("MySQL connection is closed")
+    #except Error as e:
+    #    print("Error reading data from MySQL table", e)
+    finally:
+#Fecha a conexão com o banco de dados
+        if (connection.is_connected()):
+            connection.close()
+            cursor.close()
+#Controle em Run
+            print("MySQL connection is closed")
 
-arquivo_excel.save("C:/Users/***/Documents/Posicao_Carteira.xlsx")
+#Salva a planilha com os dados no diretório
+    arquivo_excel.save("C:/Users/smarchini/Documents/Posicao_Carteira.xlsx")
 
-print("File Posicao_Carteira.xlsx CREATED")
+#Controle em Run
+    print("File Posicao_Carteira.xlsx CREATED")
+
+#Executa a função
+posicao_carteira_xl()
